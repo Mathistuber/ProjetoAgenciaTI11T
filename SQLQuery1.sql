22828ca749d64f59eecabe850cd82efd6f03d378
mathistuber <buscarmatheusdiaz@gmail.com>
2022-05-26 16:26:52 -0300

    procedores

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿USE [C:\USERS\MATHEUS.DLAGARES\SOURCE\REPOS\MATHISTUBER\PROJETOAGENCIATI11T\PROJETOAGENCIATI11T\BANCO\BDAGENCIAVIAGENS.MDF]
+GO
+
+DECLARE	@return_value Int,
+		@codigoCli int
+
+EXEC	@return_value = [dbo].[pCadastrarCliente]
+		@nomeCli = N'matheus',
+		@emailCli = N'bsbeyeyeyeyey@gmail.com',
+		@senhaCli = N'matheus',
+		@imagemCli = NULL,
+		@codigoCli = @codigoCli OUTPUT
+
+SELECT	@codigoCli as N'@codigoCli'
+
+SELECT	@return_value as 'Return Value'
+
+GO
---
+++ SQLQuery2.sql
@@ -0,0 +1,17 @@
+﻿USE [C:\USERS\MATHEUS.DLAGARES\SOURCE\REPOS\MATHISTUBER\PROJETOAGENCIATI11T\PROJETOAGENCIATI11T\BANCO\BDAGENCIAVIAGENS.MDF]
+GO
+
+DECLARE	@return_value Int,
+		@codigoFun int
+
+EXEC	@return_value = [dbo].[pCadastrarFuncionario]
+		@nomeFun = N'cdrgfggg',
+		@emailFun = N'gggggg',
+		@senhaFun = N'gggggg',
+		@codigoFun = @codigoFun OUTPUT
+
+SELECT	@codigoFun as N'@codigoFun'
+
+SELECT	@return_value as 'Return Value'
+
+GO
